482b76a93b4ac4a104cd72c81412ce01451ac08c
unknown <mittervan@gmail.com>
2021-06-09 10:35:13 +0700

    su dung toan tu

---
+++ sudungtoantu.js
@@ -0,0 +1,7 @@
+let inputwight = prompt("nhap chi dai"); let inputheight = prompt("nhap chieu rong");
+inputwight=parseInt(inputwight);
+inputheight=parseInt(inputheight)
+let area = inputwight*inputheight
+    document.write("chieu dai hinh chu nhat la: "+inputwight+"<br>");
+    document.write("chieu rong hinh chu nhat la: "+inputheight+"<br>");
+    document.write("dien tich hinh chu nhat la: "+area)
